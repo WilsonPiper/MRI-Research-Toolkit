78f919a3d2108a49fcd56b0d90beafd22676852c
WilsonPiper <wilsonxu2006@gmail.com>
2025-05-25 16:02:31 +0800

    bug fix

--- file_name_extract.py
+++ file_name_extract.py
@@ -1,14 +1,13 @@
 import os
 
-#change the following file path 
+#TODO: change the following file path name
 path = "/Users/lxl/Desktop/Summer Research/NeuroT-Map-main/lesions"
 files = os.listdir(path)
 resultant_files = []
 
 for f in files:
     length = len(f)
-    if (f[length-4:length:1] == ".nii") or (f[length-7:length:1] == ".nii.gz"):
-        resultant_files.append(f)
-
-for names in resultant_files:
-    print(f"{names} ", end= '')
+    if (f[length-4:length:1] == ".nii"):
+        print(f"{f[:length-4]}", end= ' ')
+    elif (f[length-7:length:1] == ".nii.gz"):
+        print(f"{f[:length-7]}", end = ' ')
